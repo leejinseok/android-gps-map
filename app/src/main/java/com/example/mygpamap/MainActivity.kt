16ab3a97a343d310212ea22dcdc080dc8174c6e7
leejinseok <sonaky47@gmail.com>
2023-09-13 16:45:39 +0900

    권한요청 기능

--- app/src/main/java/com/example/mygpamap/MainActivity.kt
+++ app/src/main/java/com/example/mygpamap/MainActivity.kt
@@ -1,14 +1,59 @@
 package com.example.mygpamap
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
+            val granted = remember {
+                mutableStateOf(false)
+            }
+
+            val launcher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.RequestPermission(),
+                onResult = {
+                    granted.value = it
+                })
+
+            if (granted.value) {
+                Text(text = "권한있음")
+            } else {
+                RequestPermissionScreen {
+                    launcher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun RequestPermissionScreen(onClickRequestPermission: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "권한이 존재하지 않습니다")
+        Button(onClick = { onClickRequestPermission() }) {
+            Text(text = "권한요청")
         }
     }
 }
